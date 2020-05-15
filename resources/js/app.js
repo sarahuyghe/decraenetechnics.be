bb2efd538e70d3c88e90a09dca3c407c9b079fe2
sarahuyghe <sara.huyghe@student.howest.be>
2020-05-15 11:04:09 +0200

    added temp file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,57 +1,46 @@
-// /**
-//  *  Main application javascript file
-//  *  Theme Name: Decraene Technics by digital astronaut
-//  *  Theme URI: https://decraenetechnics.be/
-//  *  Author: Tim Camerlinckx / digital astronaut
-//  *  Author URI: https://digitalastronaut.be/
-//  *  Version: 1.0.0
-//  */
+/**
+ *  Main application javascript file
+ *  Theme Name: Decraene Technics by digital astronaut
+ *  Theme URI: https://decraenetechnics.be/
+ *  Author: Tim Camerlinckx / digital astronaut
+ *  Author URI: https://digitalastronaut.be/
+ *  Version: 1.0.0
+ */
 
 // $(document).ready(function () {
-
-//   App.init();
-
+// 	App.init();
 // });
 
-
-// var $questions = document.querySelectorAll('#questions');
+// var $questions = document.querySelectorAll("#questions");
 // var App = {
+// 	/**
+// 	 * Init
+// 	 * @descr Initializes all JavaScript functionality for the application
+// 	 */
 
+// 	init: function () {
+// 		// Navigation utils
+// 		$(window).scroll(this.scrolled);
+// 		// $questions.forEach(question => question.addEventListener('click', console.log(e.currentTarget)))
+// 	},
 
-//   /**
-//    * Init
-//    * @descr Initializes all JavaScript functionality for the application
-//    */
-
-//   init: function () {
-
-//     // Navigation utils
-//     $(window).scroll(this.scrolled);
-//     // $questions.forEach(question => question.addEventListener('click', console.log(e.currentTarget)))
-
-
-//   },
-
-//   scrolled: function () {
-//     var currentScrollPosition = $(window).scrollTop();
-//     if (currentScrollPosition > 50) $('nav#meta').addClass('scrolling');
-//     else $('nav#nav-main').removeClass('scrolling');
-//   },
-
-
-
+// 	scrolled: function () {
+// 		var currentScrollPosition = $(window).scrollTop();
+// 		if (currentScrollPosition > 50) $("nav#meta").addClass("scrolling");
+// 		else $("nav#nav-main").removeClass("scrolling");
+// 	},
 // };
-// // {
+// {
 
-// //   const $questions = document.querySelectorAll('#questions');
-// //   const init = () => {
+//   const $questions = document.querySelectorAll('#questions');
+//   const init = () => {
 
-// //   };
-// //   init();
-// // }
+//   };
+//   init();
+// }
 {
-  init = () => {
-    console.log("test");
-  };
-  init();
+	init = () => {
+		console.log("test");
+	};
+	init();
 }
