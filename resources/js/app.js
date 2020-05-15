da0856d99f19710908752923ec4aa1f390244730
sarahuyghe <sara.huyghe@student.howest.be>
2020-05-15 14:18:13 +0200

    added javascript for fixed sidebar and faq list

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,40 +7,34 @@
  *  Version: 1.0.0
  */
 
-// $(document).ready(function () {
-// 	App.init();
-// });
-
-// var $questions = document.querySelectorAll("#questions");
-// var App = {
-// 	/**
-// 	 * Init
-// 	 * @descr Initializes all JavaScript functionality for the application
-// 	 */
-
-// 	init: function () {
-// 		// Navigation utils
-// 		$(window).scroll(this.scrolled);
-// 		// $questions.forEach(question => question.addEventListener('click', console.log(e.currentTarget)))
-// 	},
-
-// 	scrolled: function () {
-// 		var currentScrollPosition = $(window).scrollTop();
-// 		if (currentScrollPosition > 50) $("nav#meta").addClass("scrolling");
-// 		else $("nav#nav-main").removeClass("scrolling");
-// 	},
-// };
-// {
-
-//   const $questions = document.querySelectorAll('#questions');
-//   const init = () => {
-
-//   };
-//   init();
-// }
 {
-	init = () => {
-		console.log("test");
-	};
-	init();
+  const $questions = document.querySelectorAll(".question");
+  const $sidebar = document.querySelector(".sidebar");
+
+
+  const onClickQuestion = e => {
+    const $question = e.currentTarget
+    const $answer = $question.parentElement.querySelector(".answer")
+    if ($answer.classList.contains('hide')) {
+      $answer.classList.remove('hide')
+    } else {
+      $answer.classList.add('hide')
+    }
+  };
+
+  const onClickSidebar = () => {
+    if ($sidebar.classList.contains('sidebar-hide')) {
+      $sidebar.classList.remove('sidebar-hide')
+    } else {
+      $sidebar.classList.add('sidebar-hide')
+    }
+  }
+
+  init = () => {
+    $questions.forEach((question) =>
+      question.addEventListener(`click`, onClickQuestion)
+    );
+    $sidebar.addEventListener(`click`, onClickSidebar)
+  };
+  init();
 }
